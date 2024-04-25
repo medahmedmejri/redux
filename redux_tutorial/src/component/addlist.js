a68b7185ed16424675a8e8257558f4a696001ebf
medahmedmejri <medahmedmejri2003@gmail.com>
2024-04-25 15:49:42 +0200

    done

---
+++ redux_tutorial/src/component/addlist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function addlist() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default addlist
---
+++ redux_tutorial/src/component/list.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function list() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default list
